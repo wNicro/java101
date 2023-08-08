b16253b848dd5ffe52bd8a0df509aea1618d88c9
Orçin Özdemir <orcinozdemir95@gmail.com>
2023-08-08 22:31:15 +0300

    Initial commit

---
+++ src/Odev1.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Odev1 {
+    public static void main(String[] args){
+
+        int mat, fizik,kimya, turkce,tarih,muzik ;
+        // Scaner sıfımızı tanımladık.
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanıcan verileri alıcaz.
+        System.out.print("Matematik Notunuz :");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz :");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz :");
+        kimya = inp.nextInt();
+
+        System.out.print("Turkce Notunuz :");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz :");
+        tarih = inp.nextInt();
+
+        System.out.print("Muzik Notunuz :");
+        muzik = inp.nextInt();
+
+        int toplam = (mat+fizik+kimya+turkce+tarih+muzik);
+        double sonuc = toplam / 6.0;
+        Boolean not = sonuc >60;
+        String a =  not ? "Geçtiniz" :"Kaldınız";
+
+
+
+        System.out.println( a );
+    }
+}

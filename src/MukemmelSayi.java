8bafd528105bc5947b66696756e6a0c68a2778e2
Orçin Özdemir <orcinozdemir95@gmail.com>
2023-09-10 14:11:46 +0300

    Create MukemmelSayi.java

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args) {
+
+        Scanner ipt = new Scanner(System.in);
+        System.out.print(" Sayi Giriniz :");
+        int a = ipt.nextInt();
+
+        int b=0;
+        for(int i=1; i<a;i++){
+            if(a % i == 0){b+=i;}
+        }
+        if(b == a){
+            System.out.print(a+" Sayısı Mükemmel Sayıdır ! ");
+        }else{
+            System.out.print(a+" Sayısı Mükemmel Sayı Değildir ! ");
+        }
+    }
+}

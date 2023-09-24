7808c38174545bf86ee1eb14534e888d3fa52a24
Orçin Özdemir <orcinozdemir95@gmail.com>
2023-09-24 17:56:03 +0300

    wNicro

---
+++ Metotlar/src/UstAlma.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class UstAlma {
+    static int ustAl(int t,int n){
+        int result=1;
+
+        if(n==0){
+            return 1;
+        }
+        result = ustAl(t,n-1)*t;
+        return  result;
+    }
+    public static void main(String[] args) {
+
+        Scanner ipt = new Scanner(System.in);
+        int x=0;
+        while(x==0) {
+            System.out.print("Taban değeri giriniz : ");
+            x = ipt.nextInt();
+            System.out.println(" ");
+        }
+        System.out.print("Üs değerini giriniz  : ");
+        int y = ipt.nextInt();
+
+        System.out.println("Sonuç : " + ustAl(x,y));
+    }
+}
